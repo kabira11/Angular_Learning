7feff1d6bebf16605c7080acf1ba08998ae1b757
Pankaj Bhardwaj <pankajbhj@gmail.com>
2023-05-29 20:34:44 +0530

    Added  a componenent

---
+++ my-angualr-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { UsersComponent } from './users/users.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    UsersComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+ }
